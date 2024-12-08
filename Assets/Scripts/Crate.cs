de695717df8b80d26c39fe8674d895570f444ad1
ToppleKek <topkek@topplekek.xyz>
2024-12-08 16:09:05 -0500

    Adjust crates to make them a little less prone to flying into space

--- Assets/Scripts/Crate.cs
+++ Assets/Scripts/Crate.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+struct ConveyorData {
+    public bool has_left_collider;
+    public Conveyor component;
+}
 public class Crate : MonoBehaviour {
-    Conveyor conveyor;
-    Conveyor prevConveyor;
+    ConveyorData conveyor;
+    ConveyorData prevConveyor;
 
     Vector3 normal;
     void FixedUpdate() {
-        if (conveyor != null && conveyor.movement_enabled) {
-            transform.Translate(conveyor.direction * 0.08f);
+        if (conveyor.has_left_collider && prevConveyor.has_left_collider) {
+            conveyor.component     = null;
+            prevConveyor.component = null;
+            return;
+        }
+
+        if (conveyor.component != null && conveyor.component.movement_enabled) {
+            transform.Translate(conveyor.component.direction * 0.08f);
         }
 
         if (transform.position.y < -10.0f) {
@@ -20,12 +30,27 @@ public class Crate : MonoBehaviour {
     void OnCollisionEnter(Collision c) {
         if (c.gameObject.CompareTag("Conveyor")) {
             Conveyor temp = c.gameObject.GetComponent<Conveyor>();
-            if (temp != prevConveyor && temp != conveyor) {
-                prevConveyor = conveyor;
-                conveyor = temp;
-                normal = c.GetContact(0).normal;
-                transform.up = normal;
+            if (temp != prevConveyor.component && temp != conveyor.component) {
+                prevConveyor.component         = conveyor.component;
+                prevConveyor.has_left_collider = conveyor.has_left_collider;
+                conveyor.has_left_collider     = false;
+                conveyor.component             = temp;
+                normal                         = c.GetContact(0).normal;
+                transform.up                   = normal;
+
+                if (prevConveyor.component) {
+                    transform.Translate(prevConveyor.component.direction * 0.19f);
+                }
+                //transform.Translate(conveyor.component.direction * 0.19f);
             }
+        }
+    }
+
+    private void OnCollisionExit(Collision c) {
+        if (c.gameObject.CompareTag("Conveyor")) {
+            Conveyor collided_conveyor = c.gameObject.GetComponent<Conveyor>();
+            if      (collided_conveyor == conveyor.component)     conveyor.has_left_collider     = true;
+            else if (collided_conveyor == prevConveyor.component) prevConveyor.has_left_collider = true;
         }
     }
 }
--- Assets/Scripts/CrateSpawner.cs
+++ Assets/Scripts/CrateSpawner.cs
@@ -12,7 +12,7 @@ public class CrateSpawner : MonoBehaviour {
 
         if (t != last_t && t % 3 == 0 && spawning_enabled) {
             GameObject g = Instantiate(crate_prefab);
-            float n = Random.Range(0.5f, 0.9f);
+            float n = Random.Range(0.5f, 0.7f);
             g.transform.localScale = new Vector3(n, n, n);
             g.transform.SetPositionAndRotation(this.transform.position, Quaternion.identity);
             last_t = t;
